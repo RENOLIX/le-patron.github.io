c5b88de13d41f30ac9c47fdd83e183427f5d1cea
asoual12 <alger12c200@gmail.com>
2026-08-18 18:43:39 +0100

    Enlarge header logo without rounded frame

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
 const RECOVERY_STORAGE_KEY = "__mina_supabase_recovery_payload";
 
